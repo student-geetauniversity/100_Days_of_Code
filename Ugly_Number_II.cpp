33544003d38e6e624fda27f01dc40a7b40b40817
Khushi <154045410+student-geetauniversity@users.noreply.github.com>
2024-08-18 22:32:15 +0530

    Day 49

---
+++ Ugly_Number_II.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> ugly(n);
+        ugly[0] = 1;
+        
+        int i2 = 0, i3 = 0, i5 = 0;
+        int next2 = 2, next3 = 3, next5 = 5;
+        
+        for (int i = 1; i < n; ++i) {
+            int nextUgly = min(next2, min(next3, next5));
+            ugly[i] = nextUgly;
+            
+            if (nextUgly == next2) {
+                ++i2;
+                next2 = ugly[i2] * 2;
+            }
+            if (nextUgly == next3) {
+                ++i3;
+                next3 = ugly[i3] * 3;
+            }
+            if (nextUgly == next5) {
+                ++i5;
+                next5 = ugly[i5] * 5;
+            }
+        }
+        
+        return ugly[n - 1];
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    
+    Solution sol;
+    cout << "The " << n << "th ugly number is: " << sol.nthUglyNumber(n) << endl;
+    
+    return 0;
+}
