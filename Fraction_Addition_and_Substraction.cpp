a1d1bd340d1f9651e394a0bb767e1f83b01fc355
Khushi <154045410+student-geetauniversity@users.noreply.github.com>
2024-08-23 14:02:32 +0530

    Day 54

---
+++ Fraction_Addition_and_Substraction.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string fractionAddition(string expression) {
+    int num = 0, denom = 1;
+    int n = expression.size();
+    int i = 0;
+
+    while (i < n) {
+        int sign = 1;
+        if (expression[i] == '-') {
+            sign = -1;
+            i++;
+        } else if (expression[i] == '+') {
+            i++;
+        }
+
+        int numerator = 0;
+        while (i < n && isdigit(expression[i])) {
+            numerator = numerator * 10 + (expression[i] - '0');
+            i++;
+        }
+        numerator *= sign;
+
+        if (i < n && expression[i] == '/') {
+            i++;
+        }
+
+        int denominator = 0;
+        while (i < n && isdigit(expression[i])) {
+            denominator = denominator * 10 + (expression[i] - '0');
+            i++;
+        }
+
+        num = num * denominator + numerator * denom;
+        denom *= denominator;
+
+        int g = gcd(abs(num), abs(denom));
+        num /= g;
+        denom /= g;
+    }
+
+    return to_string(num) + '/' + to_string(denom);
+}
+
+int main() {
+    string expression;
+    cout << "Enter the fraction expression: ";
+    cin >> expression;
+
+    string result = fractionAddition(expression);
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
